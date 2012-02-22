bceaf15ff847902b195f1c04a1597aedc072623e
Wladimir Schmidt <w_schmidt_w@yahoo.com>
2012-02-22 13:51:50 +0100

    [website] new jquery login

--- new_skin/include/_header.php
+++ new_skin/include/_header.php
@@ -2,7 +2,12 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+<script src="js/slide.js" type="text/javascript"></script>
+<!--[if lte IE 6]>
+<script type="text/javascript" src="js/iepngfix/supersleight-min.js"></script>
+<![endif]-->
 <link href="./css/style.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="./style/slide.css" media="screen" rel="stylesheet" type="text/css" />
 <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
 <meta name="keywords" content="" />
 <meta name="description" content="" />
