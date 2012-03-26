84c91b2c6ec764c510f2fb2dd99c86ee80224774
WLSCH <w_schmidt_w@yahoo.com>
2012-03-26 17:22:19 +0200

    [website] registration pumped up and login exists check implemented

--- registration.php
+++ registration.php
@@ -3,18 +3,23 @@ session_start();
 
 include_once("./include/_header.php");
 
-if(isset($_GET["confirm"]) && strlen($_GET["confirm"]) == 32){
+if(isset($_GET["confirm"]) && strlen(trim($_GET["confirm"])) == 32){
   
+   include_once("./config.php");
+   // TODO: make dynamic table from config
    include_once("./include/functions/dbconnect.php"); 
    
-   $sql = "SELECT userid FROM user WHERE hash = '". $_GET["confirm"] ."'";
+   $sql = "SELECT userid 
+           FROM user 
+           WHERE hash = '". $_GET["confirm"] ."'";
    
    $result = $db->query($sql);
-   
    $row = $result->fetch();
    
    if(!empty($row)){
-      $sql = "UPDATE user SET confirmed=1, usergroupid=1 WHERE userid=". $row["userid"];
+      $sql = "UPDATE user 
+              SET confirmed=1, usergroupid=1 
+              WHERE userid=". $row["userid"];
       
       $db->exec($sql);
        
@@ -27,47 +32,145 @@ if(isset($_POST["submitted"])){
   
   include_once("./include/functions/dbconnect.php");
   
-  $USER_CREATED = 1;
+  $USER_CREATED = 0;
+  $LOGIN_EXISTS = 1;    // default 1 because of fault tolerance
+  $ERROR_REGFORM = array();
   
-  $FIRSTNAME = $_POST["firstname"];
-  $LASTNAME = $_POST["lastname"];
-  $EMAIL = $_POST["email"];
-  $LOGIN = $_POST["login"];
-  $PASSWORD = $_POST["password"];
-  $USER_TITLE = $_POST["usertitle"];
-  $CUR_TIME = time();
-  $CONFIRM_CODE = md5($EMAIL);
-  
-  $sql = "INSERT INTO user (usergroupid, firstname, lastname, 
-                                          login, password, hash, usertitle,
-                                          email, ipaddress, lastactivity, 
-                                          joindate, passworddate)
-                                          VALUES 
-                                          (0, '". $FIRSTNAME ."', '". $LASTNAME ."',
-                                          '". $LOGIN ."', '". $PASSWORD ."', '". $CONFIRM_CODE ."', '". $USER_TITLE ."',
-                                          '". $EMAIL ."', '". $_SERVER["REMOTE_ADDR"] ."', ". $CUR_TIME .",
-                                          ". $CUR_TIME .", ". $CUR_TIME .")";
-  
-  $db->exec($sql);
-    
-                                          
-  $to = $EMAIL; 
-  $subject = "Our site - Please confirm the registration"; 
-  $from = "admin@localhost"; 
+  // init
+  $USER_TITLE = '';
+  $FIRSTNAME = '';
+  $LASTNAME = '';
+  $EMAIL = '';
+  $LOGIN = '';
+  $PASSWORD = '';
       
-  $message = "Hi, ". $FIRSTNAME ." ". $LASTNAME ."!\n";
-  $message .= "Please follow this link: ";
-  $message .= "http://". $_SERVER["SERVER_NAME"] . $_SERVER["PHP_SELF"] ."?confirm=". $CONFIRM_CODE;
-   
-  $headers = "From: $from"; 
-  $sent = mail($to, $subject, $message, $headers); 
-  
-  if($sent) {
-      echo("Your mail was sent successfully"); 
-  } else {
-      die("We encountered an error sending your mail"); 
+  if(!isset($_POST["usertitle"])){
+    $ERROR_REGFORM[] = "Please, enter your title!";
+  }else{
+      
+     $_POST["usertitle"] = trim($_POST["usertitle"]);
+     if(empty($_POST["usertitle"])){
+        $ERROR_REGFORM[] = "Please, enter your title!";    
+     }
   }
   
+  if(!isset($_POST["firstname"])){
+      $ERROR_REGFORM[] = "Please, enter your firstname!";
+  }else{
+      
+     $_POST["firstname"] = trim($_POST["firstname"]);
+     if(empty($_POST["firstname"])){
+        $ERROR_REGFORM[] = "Please, enter your firstname!";    
+     }
+  }
+  
+  if(!isset($_POST["lastname"])){
+      $ERROR_REGFORM[] = "Please, enter your lastname!";
+  }else{
+    
+     $_POST["lastname"] = trim($_POST["lastname"]);
+     if(empty($_POST["lastname"])){
+        $ERROR_REGFORM[] = "Please, enter your lastname!";    
+     }
+  }
+  
+  if(!isset($_POST["email"])){
+      $ERROR_REGFORM[] = "Please, enter your E-mail!";
+  }else{
+    
+     $_POST["email"] = trim($_POST["email"]);
+                                  
+     // really better than !preg_match( "/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/", $_POST["email"] ) ????
+     if(empty($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+        $ERROR_REGFORM[] = "Please, enter valid E-mail!";    
+     }
+  }
+  
+  if(!isset($_POST["login"])){
+      $ERROR_REGFORM[] = "Please, enter your login!";
+  }else{
+    
+     $_POST["login"] = trim($_POST["login"]); 
+     if(empty($_POST["login"]) || !preg_match("/^[A-Za-z][A-Za-z0-9]*(?:_[A-Za-z0-9]+)*$/", $_POST["login"])){
+        $ERROR_REGFORM[] = "Please, enter valid login!";    
+     }
+  }
+  
+  if(!isset($_POST["password"])){
+      $ERROR_REGFORM[] = "Please, enter your password!";   
+  }else{
+    
+     $_POST["password"] = trim($_POST["password"]); 
+     if(empty($_POST["password"])){
+        $ERROR_REGFORM[] = "Please, enter your password!";   
+     } 
+  }
+  
+  if(count($ERROR_REGFORM) == 0){
+      
+      $USER_TITLE = $_POST["usertitle"];
+      $FIRSTNAME = $_POST["firstname"];
+      $LASTNAME = $_POST["lastname"];
+      $EMAIL = $_POST["email"];
+      $LOGIN = $_POST["login"];
+      $PASSWORD = $_POST["password"];
+      $CUR_TIME = time();
+      $CONFIRM_CODE = md5($EMAIL);
+      
+      $sql = "SELECT userid 
+              FROM user 
+              WHERE login = '". $LOGIN ."'";
+      
+      $result = $db->query($sql);
+      $row = $result->fetch();
+
+      if(!empty($row)){
+          $USER_CREATED = 0;
+          $LOGIN_EXISTS = 1;
+          
+          $ERROR_REGFORM[] = "That login already exists! Please choose another one.";
+          
+      }else{
+          
+          $LOGIN_EXISTS = 0;
+          
+          // we have no duplicate logins
+          // so we safe to insert new entry
+
+         $sql = "INSERT INTO user (usergroupid, firstname, lastname, 
+                                                  login, password, hash, usertitle,
+                                                  email, ipaddress, lastactivity, 
+                                                  joindate, passworddate)
+                                                  VALUES 
+                                                  (0, '". $FIRSTNAME ."', '". $LASTNAME ."',
+                                                  '". $LOGIN ."', '". $PASSWORD ."', '". $CONFIRM_CODE ."', '". $USER_TITLE ."',
+                                                  '". $EMAIL ."', '". $_SERVER["REMOTE_ADDR"] ."', ". $CUR_TIME .",
+                                                  ". $CUR_TIME .", ". $CUR_TIME .")";
+          
+          $db->exec($sql);
+          
+          $USER_CREATED = 1;  
+             
+          // compose email to user                                        
+          $to = $EMAIL; 
+          $subject = "Our site - Please confirm the registration"; 
+          $from = "admin@localhost"; 
+              
+          $message = "Hi, ". $FIRSTNAME ." ". $LASTNAME ."!\n";
+          $message .= "Please follow this link: ";
+          $message .= "http://". $_SERVER["SERVER_NAME"] . $_SERVER["PHP_SELF"] ."?confirm=". $CONFIRM_CODE;
+           
+          $headers = "From: $from"; 
+          $sent = mail($to, $subject, $message, $headers); 
+          
+          // sending was successful?
+          if($sent) {
+              echo("Your mail was sent successfully"); 
+          } else {
+              die("We encountered an error sending your mail"); 
+          } 
+      } 
+  } 
 }
   
 ?>
@@ -113,30 +216,59 @@ if(isset($_POST["submitted"])){
                             <form class="registration_form" action="./registration.php" method="post" accept-charset="UTF-8">
                                 <fieldset>
                                     <legend>Registration of new user</legend>
-                                    <label for="usertitle" >Your salutation (*): </label>
-                                    <div class="clear"></div>
-                                    <input type="text" name="usertitle" id="usertitle" maxlength="10" />
-                                    <div class="clear"></div>
                                     <label for="firstname" >Your first name (*): </label>
                                     <div class="clear"></div>
-                                    <input type="text" name="firstname" id="firstname" maxlength="50" />
+                                    <input type="text" name="firstname" id="firstname" maxlength="50" <?php
+                                        if(isset($_POST["firstname"])){
+                                            echo 'value="'. trim($_POST["firstname"]) .'" ';
+                                        }                                                                         
+                                    ?>/>
                                     <div class="clear"></div>
                                     <label for="lastname" >Your last name (*): </label>
                                     <div class="clear"></div>
-                                    <input type="text" name="lastname" id="lastname" maxlength="50" />
+                                    <input type="text" name="lastname" id="lastname" maxlength="50" <?php
+                                        if(isset($_POST["lastname"])){
+                                            echo 'value="'. trim($_POST["lastname"]) .'" ';
+                                        }                                                                         
+                                    ?>/>
                                     <div class="clear"></div>
                                     <label for="email" >E-mail address (*):</label>
                                     <div class="clear"></div>
-                                    <input type="text" name="email" id="email" maxlength="50" />
+                                    <input type="text" name="email" id="email" maxlength="50" <?php
+                                        if(isset($_POST["email"])){
+                                            echo 'value="'. trim($_POST["email"]) .'" ';
+                                        }                                                                         
+                                    ?>/>
                                     <div class="clear"></div>
                                     <label for="login" >Login (*):</label>
                                     <div class="clear"></div>
-                                    <input type="text" name="login" id="login" maxlength="50" />
+                                    <input type="text" name="login" id="login" maxlength="50" <?php
+                                        if(isset($_POST["login"])){
+                                            echo 'value="'. trim($_POST["login"]) .'" ';
+                                        }                                                                         
+                                    ?>/>
                                     <div class="clear"></div>
                                     <label for="password" >Password (*):</label>
                                     <div class="clear"></div>
-                                    <input type="password" name="password" id="password" maxlength="50" />
+                                    <input type="password" name="password" id="password" maxlength="50" <?php
+                                        if(isset($_POST["password"])){
+                                            echo 'value="'. trim($_POST["password"]) .'" ';
+                                        }                                                                         
+                                    ?>/>
                                     <div class="clear"></div>
+                                    <?php
+                                         if(count($ERROR_REGFORM) > 0){
+                                     ?>
+                                        <ul class="error_regform"><?php
+                                        
+                                        foreach($ERROR_REGFORM as $err){
+                                           echo "<li>". $err ."</li>"; 
+                                        }
+                                        
+                                        ?></ul>
+                                    <?php
+                                         }
+                                     ?>
                                     <input type="hidden" name="submitted" id="submitted" value="1" />
                                     <input type="submit" name="submit" value="Register" />
                                 </fieldset>
