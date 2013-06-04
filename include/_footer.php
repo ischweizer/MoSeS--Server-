5dac5a21e5d960f2157d4abfc31535c50373557e
Wladimir Schmidt <pr0t3ct0r@gmail.com>
2013-06-04 02:40:48 +0200

    + Preparing website for a new design. Added mock up for the main page (menu not finished yet)

--- include/_footer.php
+++ include/_footer.php
@@ -7,7 +7,7 @@
     <!-- end #footer -->
 </div>
 <script src="js/jquery-2.0.1.min.js"></script>
-<script src="js/slide.js" type="text/javascript"></script>
+<!--<script src="js/slide.js" type="text/javascript"></script>-->
 <script src="js/main.js" type="text/javascript"></script>
 <!--[if lte IE 6]>
 <script type="text/javascript" src="js/iepngfix/supersleight-min.js"></script>
--- include/_header.php
+++ include/_header.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-<link href="./css/style.css" media="screen" rel="stylesheet" type="text/css" />
-<link href="./css/slide.css" media="screen" rel="stylesheet" type="text/css" />
+<!--<link href="./css/style.css" media="screen" rel="stylesheet" type="text/css" />-->
+<!--<link href="./css/slide.css" media="screen" rel="stylesheet" type="text/css" />-->
 <link href="./css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="./css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
 <link rel="shortcut icon" type="image/x-icon" href="./favicon.ico">
