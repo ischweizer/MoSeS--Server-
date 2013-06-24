749d824dd0a371bccf8666599c9ffa02d48fa464
Zijad Maksuti <zijad.maksuti@gmail.com>
2013-06-24 17:31:49 +0200

    * fixed issue when jquery.bootpag.min.js is not executed on Google Chrome + added closing X on lightbox

--- include/_login.php
+++ include/_login.php
@@ -1,6 +1,7 @@
 <!-- Login form -->
 <div id="dim_back">
     <form id="lightbox" class="form-signin" action="./" method="post" name="login_form" accept-charset="UTF-8">
+    	<a class="boxclose" id="boxclose"></a>
         <h2 class="form-signin-heading muted">Please sign in</h2>
 <!--         <div class="control-group error"> -->
         <input type="text" class="input-block-level" placeholder="Login" name="login">
