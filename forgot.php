314404e2c76c1b4766b0778f8552323fa6e0016b
Zijad Maksuti <zijad.maksuti@gmail.com>
2013-07-03 14:37:06 +0200

    + forgot.php validation for correctly written emails

--- forgot.php
+++ forgot.php
@@ -22,9 +22,9 @@ include_once("./include/_header.php");
 			<fieldset id="forgot_fieldset">
 				<legend>Forgot your credentials? Your are on the right place!</legend>
                 <div class="control-group">
-                	<label for="email" class="control-label">Ener your Email here</label>
+                	<label for="email_for" class="control-label">Enter your Email here</label>
                     <div class="controls">
-                    	<input type="text" name="email" id="email" maxlength="50" />
+                    	<input type="text" name="email_for" id="email_for" maxlength="50" />
                     </div>
 				</div>
 				<div class="clear"></div>
@@ -48,3 +48,37 @@ include_once("./include/_header.php");
  //Import of footer
   include_once("./include/_footer.php");  
 ?>
+
+<script type="text/javascript">
+    
+    /**
+     * Validate the email entered by the user.
+     * It has to look like an email.
+     */
+     
+    $("#forgotEmailForm").validate({
+		rules:{
+			email_for:{
+					required:true,
+					email: true
+				}
+		},
+		
+		messages:{
+			email_for:{
+				required:"Enter your email address",
+				email:"Enter a valid email address"
+			}
+		},
+		errorClass: "help-inline",
+		errorElement: "span",
+		highlight:function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('success');
+			$(element).parents('.control-group').addClass('error');
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$(element).parents('.control-group').removeClass('error');
+			$(element).parents('.control-group').addClass('success');
+		}
+	});
+    </script>
