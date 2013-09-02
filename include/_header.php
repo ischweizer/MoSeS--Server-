3359d07e1b1d1f0f193d5c3ae723ebb9d8eb18f6
Wladimir Schmidt <pr0t3ct0r@gmail.com>
2013-09-02 17:47:56 +0200

    [server] + Added cache [server] + Added external file as internal :) (no more request to another server)

--- include/_header.php
+++ include/_header.php
@@ -24,20 +24,17 @@
  ?><!DOCTYPE html>
 <html lang="en">
 <head>
-<meta http-equiv="Expires" content="Fri, Jan 01 1900 00:00:00 GMT">
-<meta http-equiv="Pragma" content="no-cache">
-<meta http-equiv="Cache-Control" content="no-cache">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta http-equiv="Lang" content="en">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="author" content="">
-<meta name="description" content="">
-<meta name="keywords" content="">
+<meta name="author" content="Telecooperation (TK) Lab, TU Darmstadt">
+<meta name="description" content="MoSeS: Mobile Sensing System">
+<meta name="keywords" content="MoSeS mobile sensing system">
 <meta name="revisit-after" content="15 days">
 <link href="./css/bootstrap.min.css" rel="stylesheet" media="screen">
 <link href="./css/style.css" rel="stylesheet" media="screen">
 <link href="./css/smoothness/jquery-ui-1.10.3.custom.min.css" rel="stylesheet" media="screen">
 <link rel="icon" href="./img/favicon.png" type="image/png" />
 <!--[if lt IE 9]>
-      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <script src="./js/iepngfix/html5.js"></script>
     <![endif]-->
